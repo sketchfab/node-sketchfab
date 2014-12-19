fc1074077bc2aa2c5da561376daa6e707f5a40c5
Maurice Svay <maurice@sketchfab.com>
2014-12-20 00:54:06 +0100

    Improves error handling

--- example/upload.js
+++ example/upload.js
@@ -3,18 +3,25 @@
 var path = require('path');
 var Sketchfab = require('../lib/Sketchfab');
 
-var skfb = new Sketchfab('YOUR_API_TOKEN');
+var client = new Sketchfab('YOUR_API_TOKEN');
 
-var task = skfb.upload({
-    'file': path.resolve(__dirname, './cube.obj')
-}, function( err, task){
-    task.on('progress', function( value ){
+var task = client.upload({
+    'file': path.resolve(__dirname, './cube.obj'),
+    'private': true
+}, function(err, task) {
+
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    task.on('progress', function(value) {
         console.log('Progress:', value);
     });
-    task.on('success', function( url ){
+    task.on('success', function(url) {
         console.log('Success:', url);
     });
-    task.on('error', function( error ){
+    task.on('error', function(error) {
         console.log('Error:', error);
     });
 });
